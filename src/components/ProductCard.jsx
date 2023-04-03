7fcdbec629bf9bec2a43726dcc7dc1da0304ba82
Mohosin999 <hasanmohosin998@gmail.com>
2023-04-03 11:54:00 +0600

    frontend home page dynamic

--- src/components/ProductCard.jsx
+++ src/components/ProductCard.jsx
@@ -1,21 +1,42 @@
 import React from "react";
 import Link from "next/link";
+import Image from "next/image";
+import { getDiscountedPricePercentage } from "@/utils/helper";
 
-const ProductCard = () => {
+const ProductCard = ({ data }) => {
   return (
     <Link
       className="transform overflow-hidden bg-white duration-200 hover:scale-105 cursor-pointer"
-      href={"/product/1"}
+      href={`/products/${data.attributes.slug}`}
     >
-      <img className="w-full" src="/product-1.webp" alt="Product Name" />
+      <Image
+        width={500}
+        height={500}
+        src={data.attributes.thumbnail.data.attributes.url}
+        alt={data.attributes.name}
+      />
       <div className="p-4 text-black/[0.9]">
-        <h2 className="text-lg font-medium">Product Name</h2>
+        <h2 className="text-lg font-medium">{data.attributes.name}</h2>
         <div className="flex items-center text-black/[0.5]">
-          <p className="mr-2 text-lg font-semibold">$20.00</p>
-          <p className="text-base  font-medium line-through">$25.00</p>
-          <p className="ml-auto text-base font-medium text-green-500">
-            20% off
+          <p className="mr-2 text-lg font-semibold">
+            &#2547;{data.attributes.price}
           </p>
+
+          {/* if original price exist, then this following code will rendered */}
+          {data.attributes.original_price && (
+            <>
+              <p className="text-base  font-medium line-through">
+                &#2547;{data.attributes.original_price}
+              </p>
+              <p className="ml-auto text-base font-medium text-green-500">
+                {getDiscountedPricePercentage(
+                  data.attributes.original_price,
+                  data.attributes.price
+                )}
+                % off
+              </p>
+            </>
+          )}
         </div>
       </div>
     </Link>

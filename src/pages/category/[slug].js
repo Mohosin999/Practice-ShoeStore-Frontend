309c75fb780f8318f4f7ddde2ed9c2951859941c
Mohosin999 <hasanmohosin998@gmail.com>
2023-04-03 17:40:41 +0600

    changes

--- src/pages/category/[slug].js
+++ src/pages/category/[slug].js
@@ -1,10 +1,27 @@
 import React, { useEffect, useState } from "react";
-// import { useRouter } from "next/router";
+import { useRouter } from "next/router";
+import useSWR from "swr";
 import Wrapper from "@/components/Wrapper";
 import ProductCard from "@/components/ProductCard";
+import { fetchDataFromApi } from "@/utils/api";
 
-const Category = () => {
-  //   const router = useRouter();
+const maxResult = 3;
+
+const Category = ({ category, products, slug }) => {
+  // pagination and it's next page data fetch start
+  const [pageIndex, setPageIndex] = useState(1);
+  const { query } = useRouter();
+
+  useEffect(() => {
+    setPageIndex(1);
+  }, [query]);
+
+  const { data, error, isLoading } = useSWR(
+    `/api/products?populate=*&[filters][categories][slug][$eq]=${slug}&pagination[page]=${pageIndex}&pagination[pageSize]=${maxResult}`,
+    fetchDataFromApi,
+    { fallbackData: products }
+  );
+  // pagination and it's next page data fetch end
 
   return (
     <div className="w-full md:py-20 relative">
@@ -12,23 +29,84 @@ const Category = () => {
         {/* category page title */}
         <div className="text-center max-w-[800px] mx-auto mt-8 md:mt-0">
           <div className="text-[28px] md:text-[34px] mb-5 font-semibold leading-tight">
-            Running Shoes
+            {category?.data?.[0]?.attributes?.name}
           </div>
         </div>
 
         {/* products grid start */}
         <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-5 my-14 px-5 md:px-0">
-          <ProductCard />
-          <ProductCard />
-          <ProductCard />
-          <ProductCard />
-          <ProductCard />
-          <ProductCard />
+          {data?.data?.map((product) => {
+            return <ProductCard key={product?.id} data={product} />;
+          })}
         </div>
         {/* products grid end */}
+
+        {/* pagination button start */}
+        {data?.meta?.pagination?.total > maxResult && (
+          <div className="flex gap-3 items-center justify-center my-16 md:my-0">
+            <button
+              className={`rounded py-2 px-4 bg-black text-white disabled:bg-gray-200 disabled:text-gray-500`}
+              disabled={pageIndex === 1}
+              onClick={() => setPageIndex(pageIndex - 1)}
+            >
+              Previous
+            </button>
+
+            <span className="font-bold">{`${pageIndex} of ${
+              data && data.meta.pagination.pageCount
+            }`}</span>
+
+            <button
+              className={`rounded py-2 px-4 bg-black text-white disabled:bg-gray-200 disabled:text-gray-500`}
+              disabled={pageIndex === (data && data.meta.pagination.pageCount)}
+              onClick={() => setPageIndex(pageIndex + 1)}
+            >
+              Next
+            </button>
+          </div>
+        )}
+        {/* pagination button end */}
+        {isLoading && (
+          <div className="absolute top-0 left-0 w-full h-full bg-white/[0.5] flex flex-col gap-5 justify-center items-center">
+            <img src="/logo.svg" width={150} />
+            <span className="text-2xl font-medium">Loading...</span>
+          </div>
+        )}
       </Wrapper>
     </div>
   );
 };
+
+// getStaticPaths for dynamic routes
+export async function getStaticPaths() {
+  const category = await fetchDataFromApi("/api/categories?populate=*");
+
+  const paths = category.data.map((c) => ({
+    params: {
+      slug: c.attributes.slug,
+    },
+  }));
+
+  return {
+    paths,
+    fallback: false,
+  };
+}
+
+export async function getStaticProps({ params: { slug } }) {
+  // fetch data for specific category
+  const category = await fetchDataFromApi(
+    `/api/categories?filters[slug][$eq]=${slug}`
+  );
+
+  // fetch data for specific category products and it's pagination
+  const products = await fetchDataFromApi(
+    `/api/products?populate=*&[filters][categories][slug][$eq]=${slug}&pagination[page]=1&pagination[pageSize]=${maxResult}`
+  );
+
+  return {
+    props: { category, products, slug },
+  };
+}
 
 export default Category;

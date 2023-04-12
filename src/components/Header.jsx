6d791d3ffb5a8ee26644332b8754a7e99b1569f7
Mohosin999 <hasanmohosin998@gmail.com>
2023-04-12 15:46:02 +0600

    perfectly work everythings

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -13,13 +13,14 @@ import { useSelector } from "react-redux";
 const Header = () => {
   const [mobileMenu, setMobileMenu] = useState(false);
   const [showCatMenu, setShowCatMenu] = useState(false);
+  const [moreMenu, setMoreMenu] = useState(false);
   const [show, setShow] = useState("translate-y-0");
   const [lastScrollY, setLastScrollY] = useState(0);
   const [categories, setCategories] = useState(null);
 
   const { cartItems } = useSelector((state) => state.cart);
 
-  // scroll window and hide header logic start
+  // scroll window and hide header - logic start
   const controlNavbar = () => {
     if (window.scrollY > 200) {
       if (window.scrollY > lastScrollY && !mobileMenu) {
@@ -39,7 +40,7 @@ const Header = () => {
       window.removeEventListener("scroll", controlNavbar);
     };
   }, [lastScrollY]);
-  // scroll window and hide header logic end
+  // scroll window and hide header - logic end
 
   // fetch category data start
   useEffect(() => {
@@ -64,6 +65,11 @@ const Header = () => {
             alt="ShoeIcon"
             className="w-[40px] md:w-[60px]"
           />
+          {/* <img
+            src="/white-nike.svg"
+            alt="ShoeIcon"
+            className="w-[56px] md:w-[84px]"
+          /> */}
         </Link>
 
         {/* middle side header menu */}
@@ -71,6 +77,8 @@ const Header = () => {
           showCatMenu={showCatMenu}
           setShowCatMenu={setShowCatMenu}
           categories={categories}
+          moreMenu={moreMenu}
+          setMoreMenu={setMoreMenu}
         />
 
         {/* header mobile menu */}
@@ -129,3 +137,135 @@ const Header = () => {
 };
 
 export default Header;
+
+// import React, { useState, useEffect } from "react";
+// import Link from "next/link";
+// import { IoMdHeartEmpty } from "react-icons/io";
+// import { BsCart } from "react-icons/bs";
+// import { BiMenuAltRight } from "react-icons/bi";
+// import { VscChromeClose } from "react-icons/vsc";
+// import Wrapper from "./Wrapper";
+// import Menu from "./Menu";
+// import MenuMobile from "./MenuMobile";
+// import { fetchDataFromApi } from "@/utils/api";
+// import { useSelector } from "react-redux";
+
+// const Header = () => {
+//   const [mobileMenu, setMobileMenu] = useState(false);
+//   const [showCatMenu, setShowCatMenu] = useState(false);
+//   const [show, setShow] = useState("translate-y-0");
+//   const [lastScrollY, setLastScrollY] = useState(0);
+//   const [categories, setCategories] = useState(null);
+
+//   const { cartItems } = useSelector((state) => state.cart);
+
+//   // scroll window and hide header - logic start
+//   const controlNavbar = () => {
+//     if (window.scrollY > 200) {
+//       if (window.scrollY > lastScrollY && !mobileMenu) {
+//         setShow("-translate-y-[80px]");
+//       } else {
+//         setShow("shadow-sm");
+//       }
+//     } else {
+//       setShow("translate-y-0");
+//     }
+//     setLastScrollY(window.scrollY);
+//   };
+
+//   useEffect(() => {
+//     window.addEventListener("scroll", controlNavbar);
+//     return () => {
+//       window.removeEventListener("scroll", controlNavbar);
+//     };
+//   }, [lastScrollY]);
+//   // scroll window and hide header - logic end
+
+//   // fetch category data start
+//   useEffect(() => {
+//     fetchCategories();
+//   }, []);
+
+//   const fetchCategories = async () => {
+//     const { data } = await fetchDataFromApi("/api/categories?populate=*");
+//     setCategories(data);
+//   };
+//   // fetch category data end
+
+//   return (
+//     <header
+//       className={`w-full h-[50px] md:h-[80px] bg-white flex items-center justify-between z-20 sticky top-0 transition-transform duration-300 ${show}`}
+//     >
+//       <Wrapper className={"h-[60px] flex justify-between items-center"}>
+//         {/* left side header logo */}
+//         <Link href={"/"}>
+//           <img
+//             src="/logo.svg"
+//             alt="ShoeIcon"
+//             className="w-[40px] md:w-[60px]"
+//           />
+//         </Link>
+
+//         {/* middle side header menu */}
+//         <Menu
+//           showCatMenu={showCatMenu}
+//           setShowCatMenu={setShowCatMenu}
+//           categories={categories}
+//         />
+
+//         {/* header mobile menu */}
+//         {mobileMenu && (
+//           <MenuMobile
+//             showCatMenu={showCatMenu}
+//             setShowCatMenu={setShowCatMenu}
+//             setMobileMenu={setMobileMenu}
+//             categories={categories}
+//           />
+//         )}
+
+//         {/* right side header menu icons */}
+//         <div className="flex items-center gap-2 text-black">
+//           {/* icon start */}
+//           <div className="w-8 md:w-12 h-8 md:h-12 rounded-full flex justify-center items-center hover:bg-black/[0.05] cursor-pointer relative">
+//             <IoMdHeartEmpty className="text-[19px] md:text-[24px]" />
+//             <div className="h-[14px] md:h-[18px] min-w-[14px] md:min-w-[14px] rounded-full bg-red-600 absolute top-1 left-5 md:left-7 text-white text-[10px] md:text-[12px] flex items-center justify-center px-[2px] md:px[5px]">
+//               50
+//             </div>
+//           </div>
+//           {/* icon end */}
+
+//           {/* icon start */}
+//           <Link href={"/cart"}>
+//             <div className="w-8 md:w-12 h-8 md:h-12 rounded-full flex justify-center items-center hover:bg-black/[0.05] cursor-pointer relative">
+//               <BsCart className="text-[15px] md:text-[20px]" />
+//               {cartItems.length > 0 && (
+//                 <div className="h-[14px] md:h-[18px] min-w-[14px] md:min-w-[18px] rounded-full bg-red-600 absolute top-1 left-5 md:left-7 text-white text-[10px] md:text-[12px] flex justify-center items-center px-[2px] md:px-[5px]">
+//                   {cartItems.length}
+//                 </div>
+//               )}
+//             </div>
+//           </Link>
+//           {/* icon end */}
+
+//           {/* mobile menu icon start */}
+//           <div className="w-8 md:w-12 h-8 md:h-12 rounded-full flex md:hidden justify-center items-center hover:bg-black/[0.05] cursor-pointer relative -mr-2">
+//             {mobileMenu ? (
+//               <VscChromeClose
+//                 className="text-[16px]"
+//                 onClick={() => setMobileMenu(false)}
+//               />
+//             ) : (
+//               <BiMenuAltRight
+//                 className="text-[20px]"
+//                 onClick={() => setMobileMenu(true)}
+//               />
+//             )}
+//           </div>
+//           {/* mobile menu icon end */}
+//         </div>
+//       </Wrapper>
+//     </header>
+//   );
+// };
+
+// export default Header;
--- src/components/Menu.jsx
+++ src/components/Menu.jsx
@@ -2,66 +2,162 @@ import React from "react";
 import Link from "next/link";
 import { BsChevronDown } from "react-icons/bs";
 
-const data = [
-  { id: 1, name: "Home", url: "/" },
-  { id: 2, name: "About", url: "/about" },
-  { id: 3, name: "Categories", subMenu: true },
-  { id: 4, name: "Contact", url: "/contact" },
+const moreMenuData = [
+  { id: 1, name: "About", url: "/about" },
+  { id: 2, name: "Contact Us", url: "/contact" },
 ];
 
-const subMenuData = [
-  { id: 1, name: "Jordan", doc_count: 11 },
-  { id: 2, name: "Sneakers", doc_count: 8 },
-  { id: 3, name: "Running shoes", doc_count: 64 },
-  { id: 4, name: "Football shoes", doc_count: 107 },
-];
-
-const Menu = ({ showCatMenu, setShowCatMenu, categories }) => {
+const Menu = ({
+  showCatMenu,
+  setShowCatMenu,
+  categories,
+  moreMenu,
+  setMoreMenu,
+}) => {
   return (
     <ul className="hidden md:flex items-center gap-8 font-medium text-black">
-      {data.map((item) => {
-        return (
-          <React.Fragment key={item.id}>
-            {!!item?.subMenu ? (
-              <li
-                className="cursor-pointer flex items-center gap-2 relative"
-                onMouseEnter={() => setShowCatMenu(true)}
-                onMouseLeave={() => setShowCatMenu(false)}
-              >
-                {item.name}
-                <BsChevronDown size={14} />
-                {showCatMenu && (
-                  <ul className="bg-white absolute top-6 left-0 min-w-[250px] px-1 shadow-lg">
-                    {categories?.map(({ attributes: c, id }) => {
-                      return (
-                        <Link
-                          key={id}
-                          href={`/category/${c.slug}`}
-                          className="flex justify-between items-center"
-                          onClick={() => setShowCatMenu(false)}
-                        >
-                          <li className="h-12 flex justify-between items-center px-3 hover:bg-black/[0.03] rounded-md">
-                            {c.name}
-                          </li>
-                          <span className="opacity-50 text-sm px-3">
-                            {`(${c.products.data.length})`}
-                          </span>
-                        </Link>
-                      );
-                    })}
-                  </ul>
-                )}
-              </li>
-            ) : (
-              <li className="cursor-pointer">
-                <Link href={item?.url}>{item.name}</Link>
-              </li>
-            )}
-          </React.Fragment>
-        );
-      })}
+      <Link href="/home">Home</Link>
+      <Link href="/products">Products</Link>
+
+      {/* Categories menu design */}
+      <Link
+        href=""
+        className="flex items-center gap-1 relative"
+        onMouseEnter={() => setShowCatMenu(true)}
+        onMouseLeave={() => setShowCatMenu(false)}
+      >
+        Categories
+        {/* Render chevron icon */}
+        <BsChevronDown size={14} />
+        {/* Render subMenu if showCatMenu is true */}
+        {showCatMenu && (
+          <ul className="bg-white absolute top-6 left-0 min-w-[250px] px-1 shadow-lg">
+            {/* Map through categories and render them as Links */}
+            {categories?.map(({ attributes: c, id }) => {
+              return (
+                <Link
+                  key={id}
+                  href={`/category/${c.slug}`}
+                  className="flex justify-between items-center"
+                  onClick={() => setShowCatMenu(false)}
+                >
+                  {/* Render category name */}
+                  <li className="w-full h-12 flex justify-between items-center px-3 hover:bg-black/[0.04] rounded-md">
+                    {c.name}
+                    {/* Render number of products in category */}
+                    <span className="opacity-50 text-sm px-3">
+                      {`(${c.products.data.length})`}
+                    </span>
+                  </li>
+                </Link>
+              );
+            })}
+          </ul>
+        )}
+      </Link>
+
+      {/* More menu design */}
+      <Link
+        href=""
+        className="flex items-center gap-1 relative"
+        onMouseEnter={() => setMoreMenu(true)}
+        onMouseLeave={() => setMoreMenu(false)}
+      >
+        More
+        {/* Render chevron icon */}
+        <BsChevronDown size={14} />
+        {/* Render moreMenu if moreMenuis true */}
+        {moreMenu && (
+          <ul className="bg-white absolute top-6 left-0 min-w-[250px] px-1 shadow-lg">
+            {/* Map through moreMenuData and render them as Links */}
+            {moreMenuData.map(({ name, url, id }) => {
+              return (
+                <Link
+                  key={id}
+                  href={url || ""}
+                  className="flex justify-between items-center"
+                  onClick={() => setMoreMenu(false)}
+                >
+                  {/* Render menu item name */}
+                  <li className="w-full h-12  flex justify-center items-center px-3 hover:bg-black/[0.04] rounded-md">
+                    {name}
+                  </li>
+                </Link>
+              );
+            })}
+          </ul>
+        )}
+      </Link>
     </ul>
   );
 };
 
 export default Menu;
+
+// import React from "react";
+// import Link from "next/link";
+// import { BsChevronDown } from "react-icons/bs";
+
+// const data = [
+//   { id: 1, name: "Home", url: "/" },
+//   { id: 2, name: "Products", url: "/products" },
+//   { id: 3, name: "Categories", subMenu: true },
+//   { id: 4, name: "More", url: "/more" },
+// ];
+
+// const subMenuData = [
+//   { id: 1, name: "Jordan", doc_count: 11 },
+//   { id: 2, name: "Sneakers", doc_count: 8 },
+//   { id: 3, name: "Running shoes", doc_count: 64 },
+//   { id: 4, name: "Football shoes", doc_count: 107 },
+// ];
+
+// const Menu = ({ showCatMenu, setShowCatMenu, categories }) => {
+//   return (
+//     <ul className="hidden md:flex items-center gap-8 font-medium text-black">
+//       {data.map((item) => {
+//         return (
+//           <React.Fragment key={item.id}>
+//             {!!item?.subMenu ? (
+//               <li
+//                 className="cursor-pointer flex items-center gap-2 relative"
+//                 onMouseEnter={() => setShowCatMenu(true)}
+//                 onMouseLeave={() => setShowCatMenu(false)}
+//               >
+//                 {item.name}
+//                 <BsChevronDown size={14} />
+//                 {showCatMenu && (
+//                   <ul className="bg-white absolute top-6 left-0 min-w-[250px] px-1 shadow-lg">
+//                     {categories?.map(({ attributes: c, id }) => {
+//                       return (
+//                         <Link
+//                           key={id}
+//                           href={`/category/${c.slug}`}
+//                           className="flex justify-between items-center"
+//                           onClick={() => setShowCatMenu(false)}
+//                         >
+//                           <li className="h-12 flex justify-between items-center px-3 hover:bg-black/[0.03] rounded-md">
+//                             {c.name}
+//                           </li>
+//                           <span className="opacity-50 text-sm px-3">
+//                             {`(${c.products.data.length})`}
+//                           </span>
+//                         </Link>
+//                       );
+//                     })}
+//                   </ul>
+//                 )}
+//               </li>
+//             ) : (
+//               <li className="cursor-pointer">
+//                 <Link href={item?.url}>{item.name}</Link>
+//               </li>
+//             )}
+//           </React.Fragment>
+//         );
+//       })}
+//     </ul>
+//   );
+// };
+
+// export default Menu;
